ed7172bd02ed76733ca5c44267ede390a7a97de1
{Shimgo2008} <{Shimgo2008@gmail.com}>
2025-07-09 20:49:14 +0900

    Feat: getsteps function

---
+++ lib/healthkit/healthkit.dart
@@ -0,0 +1,38 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:health/health.dart';
+
+final health = Health();
+
+List<RecordingMethod> recordingMethodsToFilter = [];
+
+Future<int?> fetchStepData() async {
+  int? steps;
+
+  final now = DateTime.now();
+  final midnight = DateTime(now.year, now.month, now.day);
+
+  bool stepsPermission =
+      await health.hasPermissions([HealthDataType.STEPS]) ?? false;
+  if (!stepsPermission) {
+    stepsPermission =
+        await health.requestAuthorization([HealthDataType.STEPS]);
+  }
+
+  if (stepsPermission) {
+    try {
+      steps = await health.getTotalStepsInInterval(midnight, now,
+          includeManualEntry:
+              !recordingMethodsToFilter.contains(RecordingMethod.manual));
+    } catch (error) {
+      debugPrint("Exception in getTotalStepsInInterval: $error");
+    }
+
+    debugPrint('Total number of steps: $steps');
+    return steps;
+  } else {
+    debugPrint("Authorization not granted - error in authorization");
+    return null;
+  }
+}
